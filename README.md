6eaead3b320275d0c939c9bc19607717b04c37c3
Yan Pantoja <yan.pantoja@liberfly.com.br>
2020-01-28 01:49:32 -0300

    added crud routes

--- README.md
+++ README.md
@@ -8,26 +8,6 @@
 
 <blockquote align="center">“Sua única limitação é você mesmo”!</blockquote>
 
-<p align="center">
-  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/rocketseat/bootcamp-gostack-desafio-01?color=%2304D361">
-
-  <a href="https://rocketseat.com.br">
-    <img alt="Made by Rocketseat" src="https://img.shields.io/badge/made%20by-Rocketseat-%2304D361">
-  </a>
-
-  <img alt="License" src="https://img.shields.io/badge/license-MIT-%2304D361">
-
-  <a href="https://github.com/Rocketseat/bootcamp-gostack-desafio-01/stargazers">
-    <img alt="Stargazers" src="https://img.shields.io/github/stars/rocketseat/bootcamp-gostack-desafio-01?style=social">
-  </a>
-</p>
-
-<p align="center">
-  <a href="#rocket-sobre-o-desafio">Sobre o desafio</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-  <a href="#-entrega">Entrega</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-  <a href="#memo-licença">Licença</a>
-</p>
-
 ## :rocket: Sobre o desafio
 
 Crie uma aplicação para armazenar projetos e suas tarefas do zero utilizando [Express](https://expressjs.com/pt-br/).
--- index.js
+++ index.js
@@ -2,8 +2,70 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+const projects = [];
+
+server.use((req, res, next) => {
+  console.log(`Método: ${req.method}; URL: ${req.url}`)
+  console.count(`Número de requisições`)
+  next();
+});
+
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;  
+  const project = projects.find(p => p.id == id);
+
+  if(!project) {
+    return res.status(400).json({ error: "project not found"})
+  }
+
+  return next()
+}
+
 server.get('/projects', (req, res) => {
-  return res.json({ message: "teste" })
+  return res.json(projects)
+})
+
+server.post('/projects', (req,res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project)
+
+  return res.json(projects)
+
+})
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+  project.title = title;
+
+  return res.json(projects);
+})
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const projectIndex = projects.findIndex(p => p.id == id);
+  projects.splice(projectIndex, 1);
+  return res.send()
+})
+
+server.post('/projects/:id/tasks', (req,res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+  project.tasks.push(title)
+  return res.json(project)
 })
 
 server.listen(3333);
